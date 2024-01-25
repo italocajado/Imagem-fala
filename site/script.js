702048dc1388f27b338a3bd81e22cc48cddbe2a1
italocajado <italocajado@gmail.com>
2024-01-25 12:57:13 -0300

    Beta alguma coisa

--- site/script.js
+++ site/script.js
@@ -4,84 +4,70 @@ const demosSection = document.getElementById('demos');
 const enableWebcamButton = document.getElementById('webcamButton');
 const reportSection = document.getElementById('report');
 
-// Check if webcam access is supported.
+//Checkar o acesso a webcam
 function getUserMediaSupported() {
   return !!(navigator.mediaDevices &&
     navigator.mediaDevices.getUserMedia);
 }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
-// define in the next step.
+// Ativar camera
 if (getUserMediaSupported()) {
   enableWebcamButton.addEventListener('click', enableCam);
 } else {
   console.warn('getUserMedia() is not supported by your browser');
 }
 
-// Placeholder function for next step. Paste over this in the next step.
 function enableCam(event) {
 }
 
-// Enable the live webcam view and start classification.
 function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
+  // Prosseguir utilizando modelo de COCO-SSD e TensorFlow
   if (!model) {
     return;
   }
   
-  // Hide the button once clicked.
   event.target.classList.add('removed');  
   
-  // getUsermedia parameters to force video but not audio.
   const constraints = {
     video: true
   };
 
-  // Activate the webcam stream.
+  //mantendo a camera aberta
   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
     video.addEventListener('loadeddata', predictWebcam);
   });
 }
 
-// Placeholder function for next step.
 function predictWebcam() {
 }
 
-// Pretend model has loaded so we can try out the webcam code.
 var model = true;
 demosSection.classList.remove('invisible');
 
-// Store the resulting model in the global scope of our app.
+// Guardando os resultados.
 var model = undefined;
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
+//carregando modelo
 cocoSsd.load().then(function (loadedModel) {
   model = loadedModel;
-  // Show demo section now model is ready to use.
   demosSection.classList.remove('invisible');
 });
 
 var children = [];
 
 function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
+  // Classificando.
   model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
+    // remover classificações anteriores para não poluir a tela.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
     }
     children.splice(0);
     
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
+    // Loop Entre as classificações e ecibição
     for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
+      // Exibir objetos com mais de 40% de confiança!
       if (predictions[n].score > 0.40) {
         const p = document.createElement('p');
         p.innerText = predictions[n].class  + ' - com ' 
@@ -105,32 +91,28 @@ function predictWebcam() {
       }
     }
     
-    // Call this function again to keep predicting when the browser is ready.
+    // Loop dos passos anteriores.
     window.requestAnimationFrame(predictWebcam);
   });
 }
 
 const descricaoButton = document.getElementById('descricao');
 
-// Add a click event listener to the "Descrever imagem" button
+// Criando a função de descrição da imagem
 descricaoButton.addEventListener('click', () => {
-  // Only continue if the COCO-SSD has finished loading.
   if (!model) {
     return;
   }
 
-  // Get the current frame from the webcam
+  // Pega o frame que esta em exibição
   model.detect(video).then(predictions => {
-    // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
     }
     children.splice(0);
 
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
     for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
+      // Reutilização do codigo anterior!
       if (predictions[n].score > 0.40) {
         const p = document.createElement('p');
         p.innerText = predictions[n].class  + ' - com ' 
@@ -162,20 +144,19 @@ function generateReport(predictions) {
   for (let n = 0; n < predictions.length; n++) {
     report += predictions[n].class + ' - com ' 
         + Math.round(parseFloat(predictions[n].score) * 100) 
-        + '% Confiança. \n';
+        + '% de Confiança. \n';
   }
 
-  // Create a new SpeechSynthesisUtterance object with the report text.
+  // Converter o relatorio em fala
   const speech = new SpeechSynthesisUtterance(report);
 
-  // Set the language and pitch of the speech.
+  // configurando a linguagem.
   speech.lang = 'pt-BR';
   speech.pitch = 1.5;
 
-  // Speak the report using the SpeechSynthesis interface.
+  // Falando.
   window.speechSynthesis.speak(speech);
 
-  // Set the report text to the report section.
   reportSection.innerText = report;
 }
 
